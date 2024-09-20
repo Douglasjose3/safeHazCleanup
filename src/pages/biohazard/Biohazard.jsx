ef4ef1e86ca82b6016b61334f4ff1314443889dc
Douglas Rodrigues José <douglasjose3@gmail.com>
2024-09-19 21:41:26 -0700

    Removi o componente Important.

--- src/pages/biohazard/Biohazard.jsx
+++ src/pages/biohazard/Biohazard.jsx
@@ -1,12 +1,12 @@
 import { Container, Row } from "react-bootstrap";
 import Footer from "../../components/Footer/Footer";
 import Company from "../../components/MyBiohazar/Company/Company";
-import Important from "../../components/MyBiohazar/Important/Important";
+import Hire from "../../components/MyBiohazar/Hire/Hire";
 import Process from "../../components/MyBiohazar/Process/Process";
 import Remediation from "../../components/MyBiohazar/Remediation/Remediation";
 import Services from "../../components/MyBiohazar/Servies/Services";
 import MyNavBar from "../../components/MyNavBar/MyNavBar";
-import Hire from "../../components/MyBiohazar/Hire/Hire";
+
 
 function Biohazard() {
     return (
@@ -35,7 +35,7 @@ function Biohazard() {
                     </div>
 
                     <div className="mt-5">
-                        <Important />
+                        
                     </div>
 
                     <div className="mt-5">
