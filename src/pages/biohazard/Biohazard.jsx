40a1590743b7dbbb5f978b4bbcb42f195ead1b39
Douglas Rodrigues José <douglasjose3@gmail.com>
2024-09-19 21:36:49 -0700

    Deploy 1: Erro no import do Biohazard.jsx.

--- src/pages/biohazard/Biohazard.jsx
+++ src/pages/biohazard/Biohazard.jsx
@@ -1,14 +1,12 @@
 import { Container, Row } from "react-bootstrap";
 import Footer from "../../components/Footer/Footer";
 import Company from "../../components/MyBiohazar/Company/Company";
-import Important from "../../components/MyBiohazar/Important/important";
+import Important from "../../components/MyBiohazar/Important/Important";
 import Process from "../../components/MyBiohazar/Process/Process";
 import Remediation from "../../components/MyBiohazar/Remediation/Remediation";
 import Services from "../../components/MyBiohazar/Servies/Services";
 import MyNavBar from "../../components/MyNavBar/MyNavBar";
 import Hire from "../../components/MyBiohazar/Hire/Hire";
-
-
 
 function Biohazard() {
     return (
